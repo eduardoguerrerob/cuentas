2f411a4de62ee29db72c385f8e7111bd87cb44fc
eduardoguerrerob <eduardoguerrerob@gmail.com>
2021-09-27 22:36:13 +0000

    fragment

--- sapui5/webapp/controller/Main.controller.js
+++ sapui5/webapp/controller/Main.controller.js
@@ -24,7 +24,7 @@ sap.ui.define([
             //modelo de cuentas
             let jsonSolicitudeModel = new JSONModel();
             jsonSolicitudeModel.loadData("./localService/solicitudes.json", false);
-            oView.setModel(jsonCuentasModel, "jsonSolicitudeModel");
+            oView.setModel(jsonSolicitudeModel, "jsonSolicitudeModel");
         };
 
         function onFilter(){
@@ -38,7 +38,50 @@ sap.ui.define([
             oBinding.filter(aFilter);
         };
 
-        function onShowRequests(){
+        function onShowRequests(oEvent){
+            const $tableRequests=this.getView().byId("tableRequests");
+            $tableRequests.destroyItems();
+            // obtener el registro seleccionado
+            const pressedItem = oEvent.getSource();
+            const oContext = pressedItem.getBindingContext("northwind");
+            const sPath = oContext.getPath();
+            const objectContext = oContext.getObject();
+            // indice 
+            //const idx = parseInt(sPath.replace("/Employees(","").replace(")",""));
+            // ID del empleado
+            const employeeID = objectContext.EmployeeID;
+            
+            // armar lista de solicitudes del regisro seleccionado
+            let requestsItems = [];
+            let reqData = this.getView().getModel("jsonSolicitudeModel").getData().solicitudes;
+            // filtrar las solicitudes del empleado
+            let aFilters = [];
+            //aFilters.push(new Filter("empleadoId", FilterOperator.EQ, employeeID));
+            let reqFiltered = [];
+            reqFiltered = reqData.filter((elem)=>{
+                return elem.empleadoId === employeeID;
+            });
+            let aCuentas = reqFiltered[0].cuentas;
+
+            
+            for(let i in aCuentas){
+                requestsItems.push(new sap.m.ColumnListItem({
+                    cells: [
+                        new sap.m.Label({text: aCuentas[i].numero}),
+                        new sap.m.Label({text: aCuentas[i].nombre})
+                    ]
+                }));
+            };
+            let newTable = new sap.m.Table({
+                width: "auto",
+                columns: [
+                    new sap.m.Column({header: new sap.m.Label({text: "{i18n>requestNumber}"})}),
+                    new sap.m.Column({header: new sap.m.Label({text: "{i18n>requestName}"})})
+                ],
+                items: requestsItems
+            }).addStyleClass("sapUiSmallMargin");
+
+            $tableRequests.addItem(newTable);
 
         };
 
