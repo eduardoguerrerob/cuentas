6d8eebc8be139aaf89bc5a26cf3d51ea1907b1fe
eduardoguerrerob <eduardoguerrerob@gmail.com>
2021-10-12 14:49:54 +0000

    oct12 before delete tabla emp 1

---
+++ sapui5/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+     "sap/ui/core/mvc/Controller"
+],
+/**
+ * 
+ * @param {typeof sap.ui.core.mvc.Controller} Controller 
+ */
+function(Controller){
+    return Controller.extend("egb.sapui5.controller.App",{
+        onInit: function(){
+            
+        }
+    });
+})
--- sapui5/webapp/controller/Details.controller.js
+++ sapui5/webapp/controller/Details.controller.js
@@ -1,44 +1,47 @@
 // @ts-nocheck
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/Filter",
-    "sap/ui/model/FilterOperator"
+     "sap/ui/core/mvc/Controller",
+     "sap/ui/core/routing/History",
+     "sap/ui/model/json/JSONModel"
 ],
-	/**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     * @param {typeof sap.ui.model.json.JSONModel} JSONModel
-     * @param {typeof sap.ui.model.Filter} Filter
-     * @param {typeof sap.ui.model.FilterOperator} FilterOperator
-     */
-    function (Controller, JSONModel, Filter, FilterOperator) {
-        "use strict";
+/**
+ * 
+ * @param {typeof sap.ui.core.mvc.Controller} Controller 
+ * @param {typeof sap.ui.core.sap.ui.core.routing.History} History 
+ * @param {typeof sap.ui.model.json.JSONModel} JSONModel
+ */
+function(Controller, History, JSONModel){
 
-        function onInit() {
-            // vista
-            let oView = this.getView();
-            //modelo paises
-            let jsonPaisesModel = new JSONModel();
-            jsonPaisesModel.loadData("./localService/paises.json", false);
-            oView.setModel(jsonPaisesModel, "paisesModel");
+    function _onMatched(oEvent){
+        const employeeID = oEvent.getParameter("arguments").employeeID;
+        this.getView().bindElement({
+            path: "/Employees(" + employeeID + ")",
+            model: "northwind"
+        });
+    };
+
+    return Controller.extend("egb.sapui5.controller.Details",{
+        onInit: function(){
+            const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            oRouter.getRoute("RouteDetails").attachPatternMatched(_onMatched, this);
             //modelo de cuentas
             let jsonSolicitudeModel = new JSONModel();
             jsonSolicitudeModel.loadData("./localService/solicitudes.json", false);
-            oView.setModel(jsonSolicitudeModel, "jsonSolicitudeModel");
-        };
+            this.getView().setModel(jsonSolicitudeModel, "jsonSolicitudeModel");
+        },
 
-        function onFilter() {
-            let paises = this.getView().getModel("paisesModel").getData();
-            let aFilter = [];
-            if (paises.paisSeleccionado !== "" && paises.paisSeleccionado !== undefined) {
-                aFilter.push(new Filter("Country", FilterOperator.EQ, paises.paisSeleccionado));
+        onBack: function(oEvent){
+            const oHistory = History.getInstance();
+            const sPrevious = oHistory.getPreviousHash();
+            if(sPrevious !== undefined){
+                window.history.go(-1);
             }
-            let $tableEmp = this.getView().byId("tableEmp");
-            let oBinding = $tableEmp.getBinding("items");
-            oBinding.filter(aFilter);
-        };
-
-        function onShowRequests(oEvent) {
+            else{
+                const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                oRouter.navTo("RouteMain");
+            }
+        },
+        onShowRequests: function(oEvent) {
             const $tableRequests = this.getView().byId("tableRequests");
             $tableRequests.destroyItems();
             // obtener el registro seleccionado
@@ -87,22 +90,6 @@ sap.ui.define([
                 let newText = new sap.m.Text({text: "{i18n>noRequests}"});
                 $tableRequests.addItem(newText);
             }
-
-        };
-
-        function toDetails(oEvent){
-            const employeeID = oEvent.getSource().getBindingContext("northwind").getObject().EmployeeID;
-            const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-            oRouter.navTo("RouteDetails",{
-                employeeID: employeeID
-            });
-        };
-
-        var Main = Controller.extend("egb.sapui5.controller.Main", {});
-
-        Main.prototype.onInit = onInit;
-        Main.prototype.onFilter = onFilter;
-        Main.prototype.onShowRequests = onShowRequests;
-        Main.prototype.toDetails = toDetails;
-        return Main;
+        }
     });
+})
