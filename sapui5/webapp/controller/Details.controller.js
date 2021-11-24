d9e4ad6a21d89440856ce4e437bac70ff48a1d31
eduardoguerrerob <eduardoguerrerob@gmail.com>
2021-11-24 15:37:51 +0000

    20211124: dialog

--- sapui5/webapp/controller/Details.controller.js
+++ sapui5/webapp/controller/Details.controller.js
@@ -1,106 +1,80 @@
 // @ts-nocheck
 sap.ui.define([
-     //"sap/ui/core/mvc/Controller",
-     "egb/sapui5/controller/Base.controller",
-     "sap/ui/core/routing/History",
-     "sap/ui/model/json/JSONModel"
+    //"sap/ui/core/mvc/Controller",
+    "egb/sapui5/controller/Base.controller",
+    "sap/ui/core/routing/History",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/Fragment"
 ],
-/**
- * 
- * @param {typeof sap.ui.core.mvc.Controller} Controller 
- * @param {typeof sap.ui.core.sap.ui.core.routing.History} History 
- * @param {typeof sap.ui.model.json.JSONModel} JSONModel
- */
-function(Base, History, JSONModel){
+    /**
+     * 
+     * @param {typeof sap.ui.core.mvc.Controller} Controller 
+     * @param {typeof sap.ui.core.sap.ui.core.routing.History} History 
+     * @param {typeof sap.ui.model.json.JSONModel} JSONModel
+     * @param {typeof sap.ui.core.Fragment} Fragment
+     */
+    function (Base, History, JSONModel, Fragment) {
 
-    function _onMatched(oEvent){
-        const employeeID = oEvent.getParameter("arguments").employeeID;
-        this.getView().bindElement({
-            path: "/Employees(" + employeeID + ")",
-            model: "northwind"
-        });
-    };
+        function _onMatched(oEvent) {
+            const employeeID = oEvent.getParameter("arguments").employeeID;
+            this.getView().bindElement({
+                path: "/Employees(" + employeeID + ")",
+                model: "northwind"
+            });
+        };
 
-    return Base.extend("egb.sapui5.controller.Details",{
-        onInit: function(){
-            const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-            oRouter.getRoute("RouteDetails").attachPatternMatched(_onMatched, this);
-            //modelo de cuentas
-            let jsonSolicitudeModel = new JSONModel();
-            jsonSolicitudeModel.loadData("./localService/solicitudes.json", false);
-            this.getView().setModel(jsonSolicitudeModel, "jsonSolicitudeModel");    
-        },
+        return Base.extend("egb.sapui5.controller.Details", {
+            onInit: function () {
+                const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                oRouter.getRoute("RouteDetails").attachPatternMatched(_onMatched, this);
+                //modelo de cuentas
+                let jsonSolicitudeModel = new JSONModel();
+                jsonSolicitudeModel.loadData("./localService/solicitudes.json", false);
+                this.getView().setModel(jsonSolicitudeModel, "jsonSolicitudeModel");
+            },
 
-        onBeforeRendering: function() {
-            alert("onBeforeRendering")
-            const $tableRequests = this.getView().byId("tableRequests");
-           $tableRequests.destroyItems();
-        },
+            onBeforeRendering: function () {
+                alert("onBeforeRendering")
+                const $tableRequests = this.getView().byId("tableRequests");
+                $tableRequests.destroyItems();
+            },
 
-        onBack: function(oEvent){
+            onBack: function (oEvent) {
 
- const $tableRequests = this.getView().byId("tableRequests");
+                const $tableRequests = this.getView().byId("tableRequests");
                 $tableRequests.destroyItems();
 
-            const oHistory = History.getInstance();
-            const sPrevious = oHistory.getPreviousHash();
-            if(sPrevious !== undefined){
-                window.history.go(-1);
+                const oHistory = History.getInstance();
+                const sPrevious = oHistory.getPreviousHash();
+                if (sPrevious !== undefined) {
+                    window.history.go(-1);
+                }
+                else {
+                    const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                    oRouter.navTo("RouteMain");
+                }
+            },
+
+            onCreateRequests: function() {
+                if(!this.byId("newRequestDialog")){
+                    const oView = this.getView();
+                    Fragment.load({
+                        id: oView.getId(),
+                        name: "egb.sapui5.fragment.NewRequestDialog",
+                        controller: this
+                    }).then(function(oDialog){
+                        oView.addDependent(oDialog);
+                        oDialog.open();
+                    });
+                }
+                else{
+                    this.byId("newRequestDialog").open();
+                }
+            },
+
+            onCloseNewReqDialog: function(){
+                this.byId("newRequestDialog").close();
             }
-            else{
-                const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-                oRouter.navTo("RouteMain");
-            }
-        }
-        // onShowRequests: function(oEvent) {
-        //     const $tableRequests = this.getView().byId("tableRequests");
-        //     $tableRequests.destroyItems();
-        //     // obtener el registro seleccionado
-        //     const pressedItem = oEvent.getSource();
-        //     const oContext = pressedItem.getBindingContext("northwind");
-        //     const sPath = oContext.getPath();
-        //     const objectContext = oContext.getObject();
-        //     // indice 
-        //     //const idx = parseInt(sPath.replace("/Employees(","").replace(")",""));
-        //     // ID del empleado
-        //     const employeeID = objectContext.EmployeeID;
-
-        //     // armar lista de solicitudes del regisro seleccionado
-        //     let requestsItems = [];
-        //     let reqData = this.getView().getModel("jsonSolicitudeModel").getData().solicitudes;
-        //     // filtrar las solicitudes del empleado
-        //     let aFilters = [];
-        //     //aFilters.push(new Filter("empleadoId", FilterOperator.EQ, employeeID));
-        //     let reqFiltered = [];
-        //     reqFiltered = reqData.filter((elem) => {
-        //         return elem.empleadoId === employeeID;
-        //     });
-        //     let aCuentas = reqFiltered[0].cuentas;
-
-        //     if (aCuentas.length > 0) {
-        //         for (let i in aCuentas) {
-        //             requestsItems.push(new sap.m.ColumnListItem({
-        //                 cells: [
-        //                     new sap.m.Label({ text: aCuentas[i].numero }),
-        //                     new sap.m.Label({ text: aCuentas[i].nombre })
-        //                 ]
-        //             }));
-        //         };
-        //         let newTable = new sap.m.Table({
-        //             width: "auto",
-        //             columns: [
-        //                 new sap.m.Column({ header: new sap.m.Label({ text: "{i18n>requestNumber}" }) }),
-        //                 new sap.m.Column({ header: new sap.m.Label({ text: "{i18n>requestName}" }) })
-        //             ],
-        //             items: requestsItems
-        //         }).addStyleClass("sapUiSmallMargin");
-
-        //         $tableRequests.addItem(newTable);
-        //     }
-        //     else {
-        //         let newText = new sap.m.Text({text: "{i18n>noRequests}"});
-        //         $tableRequests.addItem(newText);
-        //     }
-        // }
-    });
-})
+            
+        });
+    })
